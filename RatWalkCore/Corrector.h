75428394ddf0b952be903db11fd72399d0f34a40
Cristian David González González <cristian_david_g@live.com>
2017-04-26 17:48:56 -0500

    Correction techniques moved from RatWalkCore::Project to RatWalkCore::Corrector

--- RatWalkCore/Corrector.h
+++ RatWalkCore/Corrector.h
@@ -1,11 +1,23 @@
 #ifndef RATWALKCORE_CORRECTOR_H
 #define RATWALKCORE_CORRECTOR_H
 
+#include <string>
+#include <opencv2/opencv.hpp>
+
+#include "RatWalkCore/Video.h"
+
 namespace RatWalkCore {
 
 class Corrector {
 public:
-   Corrector();
+   Corrector(std::vector<Video> &VideoToAnalyze);
+   void performCorrection(std::string targetFilename);
+private:
+   std::vector<Video> &VideoToAnalyze;
+   cv::Mat TargetImage, TargetImageGray,
+           Image1,     Image2,       Image3,
+           ImageLeft,  ImageMiddle,  ImageRight,
+           ImageLeftG, ImageMiddleG, ImageRightG, TargetImageGrayG;
 };
 
 } // namespace RatWalkCore
